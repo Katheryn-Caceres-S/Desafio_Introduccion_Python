445901ddcee4be2299998687fffac106acc0815a
Katheryn-Caceres-S <kcs.ingcivil@gmail.com>
2025-05-11 21:16:05 +0000

    guardando

---
+++ escape.py
@@ -0,0 +1,10 @@
+#Velocidad de Escape en [m/s].
+ve = velocidad_escape
+
+#constante gravitacional en [m/s2]
+g = float (input("introduce la Constante Gravitacional g"))
+
+# radio del planeta en [m].
+r = int (input ("Introduce el radio r"))
+
+# 𝑉 𝑒 =  raiz 2 g r
